b485fc5c09dcef42dccf67e6df2b473468aa0784
Shubhangi-R <161036693+Shubhangi-R@users.noreply.github.com>
2024-02-23 20:04:49 +0530

    add python file

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+def is_palindrome(s):
+    return s == s[::-1]
+
+# Test the function
+input_string = input("Enter a string: ")
+if is_palindrome(input_string):
+    print("The string is a palindrome.")
+else:
+    print("The string is not a palindrome.")
+
